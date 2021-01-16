f1ab3c2262c1aa0eb2a586b956ec82b4a730f5af
Pablo Lemos <plemos91@gmail.com>
2021-01-16 16:32:18 +0000

    Added real data

--- SIR_model.py
+++ SIR_model.py
@@ -1,6 +1,25 @@
 import numpy as np
+import pandas
+import math
 from scipy.integrate import odeint
 from cobaya.likelihood import Likelihood
+
+def read_data(path, country = 'United Kingdom'):
+    df = pandas.read_csv(path)
+
+    # Extract specific country. The -1 keeps only the whole country, 
+    # as opposed to just a region. The 4: keeps eliminates the entries
+    # that are not part of the time series
+    data = df[df['Country/Region'] == country].iloc[-1][4:]
+
+    # Convert into a numpy array
+    data = data.to_numpy()
+
+    # Find when first case occurs
+    i_firstcase = np.where(data>0)[0][0]
+    data = data[i_firstcase:]
+
+    return data
 
 # The SIR model differential equations.
 def deriv_SIR(y, t, beta, gamma):
@@ -26,24 +45,20 @@ def integrate_SIR(S0, I0, R0, ndays, beta, gamma):
     S, I, R = ret.T
     return S, I, R
 
-# Generate fake data
+# Read the data
+I_data = read_data('./data/time_series_covid19_confirmed_global.csv')
 
 # Total population, N.
-N = 1000
+N = 66.65*1e6 # Approximate population of the UK
 # Number of days.
-ndays = 160
+ndays = len(I_data)
 # Initial number of infected and recovered individuals, I0 and R0.
-I0, R0 = 1, 0
+I0, R0 = I_data[0], 0
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0
-# Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 0.2, 1./10 
-
-_, I_data_noiseless, _ = integrate_SIR(S0, I0, R0, ndays, beta, gamma)
-I_data = np.random.normal(I_data_noiseless, 0.1)
 
 def sir_logp(beta, gamma):
     _, I_theory, _ = integrate_SIR(S0, I0, R0, ndays, beta, gamma)
 
-    chi2 = np.sum((I_theory - I_data)**2.)
+    chi2 = np.sum((I_theory - I_data)**2./I_theory**2)
     return -chi2 / 2
