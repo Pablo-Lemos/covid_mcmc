527f12504e18579369b54ddde8b50bad277e63bb
Pablo Lemos <plemos91@gmail.com>
2021-01-25 18:08:08 +0000

    Fixed cummulative distribution problem

--- SIR_model.py
+++ SIR_model.py
@@ -18,7 +18,9 @@ def read_data(path, country = 'United Kingdom'):
     --------
     data: nd.array
       The data extracted from the file
-
+    firstcase: nd.array
+      The index corresponding to the first non-zero element
+   
     '''
     df = pandas.read_csv(path)
 
@@ -30,11 +32,13 @@ def read_data(path, country = 'United Kingdom'):
     # Convert into a numpy array
     data = data.to_numpy()
 
-    # Find when first case occurs
-    i_firstcase = np.where(data>0)[0][0]
-    data = data[i_firstcase:]
+    # The data is cummulative, undo that:
+    data = data[1:] - data[:-1]
 
-    return data
+    # Find when first case occurs
+    firstcase = np.where(data>0)[0][0]
+
+    return data, firstcase
 
 def deriv_SIR(y, t, beta, gamma):
     ''' The SIR model differential equations
@@ -110,22 +114,26 @@ def logp_SIR(beta, gamma):
       The log likelihood (-2*chisq)
     '''
   
-    _, I_theory, _ = integrate_SIR(S0, I0, R0, ndays, beta, gamma)
+    _, I_theory, _= integrate_SIR(S0, I0, R0, ndays, beta, gamma)
+    
+    epsilon = 1.e-10 # To avoid dividing by zero
+    chi2_I = np.sum((I_theory - I_data)**2./(I_theory+epsilon)**2)
+    return -0.5*(chi2_I)
 
-    chi2 = np.sum((I_theory - I_data)**2./I_theory**2)
-    return -chi2 / 2
 
-# Read the data
-I_data = read_data('./data/time_series_covid19_confirmed_global.csv', country = 'United Kingdom')
+if __name__ == '__main__':
+    # Read the data
+    I_data, i_firstcase = read_data('./data/time_series_covid19_confirmed_global.csv', country = 'United Kingdom')
+    I_data = I_data[i_firstcase:]
 
-# Total population, N.
-N = 66.65*1e6 # Approximate population of the UK
-# Number of days.
-ndays = len(I_data)
-# Initial number of infected and recovered individuals, I0 and R0.
-I0, R0 = I_data[0], 0
-# Everyone else, S0, is susceptible to infection initially.
-S0 = N - I0 - R0
+    # Total population, N.
+    N = 66.65*1e6 # Approximate population of the UK
+    # Number of days.
+    ndays = len(I_data)
+    # Initial number of infected and recovered individuals, I0 and R0.
+    I0, R0 = I_data[0], 0
+    # Everyone else, S0, is susceptible to infection initially.
+    S0 = N - I0 - R0
 
 
 
