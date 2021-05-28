422923a5bdfb2c0a1bc8f4efad26ad970b5ab909
Pablo Lemos <plemos91@gmail.com>
2021-05-28 13:44:39 +0100

    Final version

--- SIR_model.py
+++ SIR_model.py
@@ -45,56 +45,7 @@ def read_data(path, country = 'United Kingdom', undo_cumulative = True):
 
     return data, firstcase
 
-def deriv_SIR(y, t, beta, gamma, q, mu):
-    ''' The SIR model differential equations
-
-    Parameters: 
-    -----------
-    y: tuple
-      A tuplee containing S, I, R  the number of susceptible, infected
-      and recovered respectively
-    t: float
-      Time
-    beta, gamma: floats
-      The parameters of the model
-
-    Returns:
-    --------
-    dSdt, dIdt, dRdt: floats
-      The derivatives of S, I, R with respect to time.
-    '''
-    
-    # First lockdown
-    if 55<=t<=136:
-      qq = q
-    elif 279<=t<=306:
-      qq = q
-    elif t>=341:
-      qq = q
-    else:
-      qq = 0
-
-    #qq = q*(np.exp(-(t-95.5)**2/2./(40.5/2)**2) + np.exp(-(t-292.5)**2/2./(15/2)**2) + np.exp(-(t-393)**2/2./(52/2)**2))
-
-    S, I, Q, R, D = y
-    
-    # Total population 
-    N = S + I + Q + R + D
-    #beta /= N
-    
-    # Assume that the incubation period is the same quarantined or not
-    gammap = gamma
-    
-    # From https://www.sciencedirect.com/science/article/pii/S2468042720300439
-    # For COVID 19, qprime can be set to zero
-    dSdt = -beta * S * I / N
-    dIdt = beta * S * I / N  - gamma * I - qq*I - mu*I
-    dQdt = qq * I - gammap*Q
-    dRdt = gamma * I + gammap*Q
-    dDdt = mu*I
-    return dSdt, dIdt, dQdt, dRdt, dDdt
-
-def integrate_SIR(S0, I0, Q0, R0, D0, ndays, beta, gamma, q, mu, quarantine_model = 'simple'):
+def integrate_SIR(S0, I0, Q0, R0, D0, ndays, beta, gamma, q, mu, model='simple'):
     ''' Integrate the SIR model equations
   
     Parameters:
@@ -120,13 +71,18 @@ def integrate_SIR(S0, I0, Q0, R0, D0, ndays, beta, gamma, q, mu, quarantine_mode
     y0 = S0, I0, Q0, R0, D0
 
     # Integrate the SIR equations over the time grid, t.
-    ret = odeint(deriv_SIR, y0, t, args=(beta, gamma, q, mu, quarantine_model))
+    if model=='simple':
+        ret = odeint(deriv_SIR, y0, t, args=(beta, gamma, q, mu))
+    elif model=='global':
+        ret = odeint(deriv_SIR_global, y0, t, args=(beta, gamma, q, mu))
+
+
     S, I, Q, R, D = ret.T
     return S, I, Q, R, D
     
 
 
-def deriv_SIR(y, t, beta, gamma, q, mu, quarantine_model = 'simple'):
+def deriv_SIR(y, t, beta, gamma, q, mu):
     ''' The SIR model differential equations
 
     Parameters: 
@@ -144,42 +100,62 @@ def deriv_SIR(y, t, beta, gamma, q, mu, quarantine_model = 'simple'):
     dSdt, dIdt, dRdt: floats
       The derivatives of S, I, R with respect to time.
     '''
-    
-    if quarantine_model == 'step':
-      # First lockdown
-      if 55<=t<=136:
-        qq = q
-      elif 279<=t<=306:
-        qq = q
-      elif t>=341:
-        qq = q
-      else:
-        qq = 0
-    elif quarantine_model == 'simple':
-      qq = q 
-    elif quarantine_model == 'gaussian':
-      qq = q*(np.exp(-(t-95.5)**2/2./(40.5/2)**2) + np.exp(-(t-292.5)**2/2./(15/2)**2) + np.exp(-(t-393)**2/2./(52/2)**2))
-    else:
-      print('UNKNOWN QUARANTINE MODEL')
 
     S, I, Q, R, D = y
     
     # Total population 
     N = S + I + Q + R + D
     
-    # Assume that the incubation period is the same quarantined or not
-    gammap = gamma
-    
     # From https://www.sciencedirect.com/science/article/pii/S2468042720300439
     # For COVID 19, qprime can be set to zero
     dSdt = -beta * S * I / N
-    dIdt = beta * S * I / N  - gamma * I - qq*I - mu*I
-    dQdt = qq * I - gammap*Q
-    dRdt = gamma * I + gammap*Q
+    dIdt = beta * S * I / N  - gamma * I - q*I - mu*I
+    dQdt = q * I - gamma*Q
+    dRdt = gamma * I + gamma*Q
     dDdt = mu*I
     return dSdt, dIdt, dQdt, dRdt, dDdt
 
-def logp_SIR(lbeta, lgamma, lq, lmu):
+def deriv_SIR_global(y, t, beta, gamma, q, mu):
+    ''' The SIR model differential equations
+
+    Parameters: 
+    -----------
+    y: tuple
+      A tuplee containing S, I, R  the number of susceptible, infected
+      and recovered respectively
+    t: float
+      Time
+    beta, gamma: floats
+      The parameters of the model
+
+    Returns:
+    --------
+    dSdt, dIdt, dRdt: floats
+      The derivatives of S, I, R with respect to time.
+    '''
+    S, I, Q, R, D = y
+    
+    # Total population 
+    N = S + I + Q + R + D
+    
+    # From https://www.sciencedirect.com/science/article/pii/S2468042720300439
+    # For COVID 19, qprime can be set to zero
+    if q>=0:
+      dSdt = -beta * S * I / N - q*S
+      dIdt = beta * S * I / N  - gamma * I - mu*I 
+      dQdt = q * S
+      dRdt = gamma * I 
+      dDdt = mu*I
+    else:
+      dSdt = -beta * S * I / N - q*Q
+      dIdt = beta * S * I / N  - gamma * I - mu*I
+      dQdt = q * Q
+      dRdt = gamma * I 
+      dDdt = mu*I       
+    return dSdt, dIdt, dQdt, dRdt, dDdt
+
+
+def logp_SIR(lbeta, lgamma, q, lmu):
     ''' 
     Calculate the log likelihood for the SIR model given parameters
 
@@ -195,10 +171,10 @@ def logp_SIR(lbeta, lgamma, lq, lmu):
     '''
     beta = np.exp(lbeta)
     gamma = np.exp(lgamma)
-    q = np.exp(lq)
+    #q = np.exp(lq)
     mu = np.exp(lmu)
     
-    _, I_theory, _, _, D_theory = integrate_SIR(S0, I0, Q0, R0, D0, ndays, beta, gamma, q, mu, quarantine_model='step')
+    _, I_theory, _, _, D_theory = integrate_SIR(S0, I0, Q0, R0, D0, ndays, beta, gamma, q, mu, model='simple')
     
     I_theory = np.clip(I_theory, a_min = 1e-1, a_max = None)
     D_theory = np.clip(D_theory, a_min = 1e-1, a_max = None)
@@ -232,6 +208,33 @@ def logp_SIR_noq(lbeta, lgamma, lmu):
     pD = D_data*np.log((D_theory).astype(np.float32)) - D_theory - D_data.astype(np.float32)*np.log(D_data.astype(np.float32)) + D_data.astype(np.float32)
     return np.sum(pI+pD)
 
+def logp_SIR_global(lbeta, lgamma, q, lmu):
+    ''' 
+    Calculate the log likelihood for the SIR model given parameters
+
+    Parameters: 
+    -----------
+    beta, gamma: floats
+      The parameters of the model
+
+    Returns:
+    --------
+    logp: float
+      The log likelihood (-2*chisq)
+    '''
+    beta = np.exp(lbeta)
+    gamma = np.exp(lgamma)
+    mu = np.exp(lmu)
+    
+    _, I_theory, _, _, D_theory = integrate_SIR(S0, I0, Q0, R0, D0, ndays, beta, gamma, q, mu, model='global')
+    
+    I_theory = np.clip(I_theory, a_min = 1e-1, a_max = None)
+    D_theory = np.clip(D_theory, a_min = 1e-1, a_max = None)
+    pI = I_data*np.log((I_theory).astype(np.float32)) - I_theory - I_data.astype(np.float32)*np.log(I_data.astype(np.float32)) + I_data.astype(np.float32)
+    pD = D_data*np.log((D_theory).astype(np.float32)) - D_theory - D_data.astype(np.float32)*np.log(D_data.astype(np.float32)) + D_data.astype(np.float32)
+    return np.sum(pI+pD)
+
+
 if __name__ == 'main':
     print('This file is to be used with cobaya, not executed')
 
@@ -239,37 +242,38 @@ if __name__ == 'SIR_model':
     # Read the data
     try:
         I_data, i_firstcase = read_data('./data/time_series_covid19_confirmed_global.csv', country = 'United Kingdom')
-        I_data = I_data[i_firstcase:]
-
         D_data, _ = read_data('./data/time_series_covid19_deaths_global.csv', country = 'United Kingdom', undo_cumulative=False)
-        D_data = D_data[i_firstcase:]
-        D_data = np.clip(D_data, a_min = 1e-1, a_max = None)
+    except:
+        I_data, i_firstcase = read_data('../data/time_series_covid19_confirmed_global.csv', country = 'United Kingdom')
+        D_data, _ = read_data('../data/time_series_covid19_deaths_global.csv', country = 'United Kingdom', undo_cumulative=False)
 
-        # Correct two big outliers
-        I_data[152] = I_data[151]
-        I_data[153] = I_data[154]
+    I_data = I_data[i_firstcase:]
+    D_data = D_data[i_firstcase:]
+    D_data = np.clip(D_data, a_min = 1e-1, a_max = None)
 
-        # Total population, N.
-        N = 66.65*1e6 # Approximate population of the UK
-        # Number of days.
-        ndays = len(I_data)
+    # Correct two big outliers
+    #I_data[152] = I_data[151]
+    #I_data[153] = I_data[154]
+    I_data= I_data[:150]
+    D_data= D_data[:150]
 
-        # Initial number of infected and recovered individuals, I0 and R0.
-        I0, Q0, R0, D0 = I_data[0], 0, 0, 0
-        # Everyone else, S0, is susceptible to infection initially.
-        S0 = N - I0 - Q0 - R0 - D0
+    # Total population, N.
+    N = 66.65*1e6 # Approximate population of the UK
+    # Number of days.
+    ndays = len(I_data)
 
-        # Each person is infected for approximately 14 days
-        Itot = np.zeros_like(I_data)
-        for i in range(ndays):
-            if i<13: 
-                Itot[i] = np.sum(I_data[:i+1])
-            else:
-                Itot[i] = np.sum(I_data[i-13:i+1])
+    # Initial number of infected and recovered individuals, I0 and R0.
+    I0, Q0, R0, D0 = I_data[0], 0, 0, 0
+    # Everyone else, S0, is susceptible to infection initially.
+    S0 = N - I0 - Q0 - R0 - D0
 
-        I_data = Itot
+    # Each person is infected for approximately 14 days
+    Itot = np.zeros_like(I_data)
+    for i in range(ndays):
+        if i<13: 
+            Itot[i] = np.sum(I_data[:i+1])
+        else:
+            Itot[i] = np.sum(I_data[i-13:i+1])
 
-    except: 
-        print('Data file not found')
-
+    I_data = Itot
 
